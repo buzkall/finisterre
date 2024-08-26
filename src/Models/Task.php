833290879f6419c4ea8351a64a258de498e2505a
buzkall <buzkall@gmail.com>
2024-08-26 20:42:57 +0200

    Add task factory

--- src/Models/Task.php
+++ src/Models/Task.php
@@ -3,16 +3,14 @@
 namespace Buzkall\Finisterre\Models;
 
 //use App\Models\User;
+use Buzkall\Finisterre\Database\Factories\TaskFactory;
 use Buzkall\Finisterre\Enums\TaskPriorityEnum;
 use Buzkall\Finisterre\Enums\TaskStateEnum;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    use HasFactory;
-
     public $fillable = ['title', 'description', 'state', 'priority', 'due_at', 'completed_at', 'created_by_user_id', 'assigned_to_user_id'];
     protected $casts = [
         'state'        => TaskStateEnum::class,
@@ -35,19 +33,25 @@ class Task extends Model
         });
     }
 
+    protected static function newFactory()
+    {
+        return new TaskFactory;
+    }
+
     public function getTable()
     {
         return config('finisterre.table_name');
     }
 
     /*
-        public function creator(): BelongsTo
-        {
-            return $this->belongsTo(User::class, 'created_by_user_id');
-        }
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by_user_id');
+    }
 
-        public function assignee(): BelongsTo
-        {
-            return $this->belongsTo(User::class, 'assigned_to_user_id');
-        }*/
+    public function assignee(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assigned_to_user_id');
+    }
+    */
 }
